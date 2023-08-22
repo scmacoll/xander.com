5fb298ea6d662dc702f62ab74fb07f5323353d63
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-08-22 11:35:07 +1000

    checkout page draft blocking

--- src/app/components/Checkout-Page-Content/Checkout-Page-Content.tsx
+++ src/app/components/Checkout-Page-Content/Checkout-Page-Content.tsx
@@ -1,10 +1,42 @@
 import styles from './Checkout-Page-Content.module.scss';
 import React from 'react';
+import classNames from 'classnames';
 
 
 
 const CheckoutPageContent: React.FC = () => {
-  return <div>hello world!</div>
+  return (
+    <div id="pageContainer"
+      className="flex justify-between w-78% h-screen pt-12 mx-auto 
+      border-d-white"
+    >
+      <div id="paymentDetailsWrapper"
+        className="flex flex-col w-51.5%
+        border-blue"
+      >
+        <div id="checkoutTitle"
+          className="block pt-1 pb-5 
+          border-green">
+          <div className="flex pb-1">
+            <h1>Xandria</h1>
+          </div>
+          <div className="flex">
+            <h4>Shipping ≥ Payment</h4>
+          </div>
+        </div>
+        <div className="flex">express checkout container</div>
+        <div className="flex">contact details container</div>
+      </div>
+      <div
+        id="cartDetailsWrapper"
+        className="flex flex-col w-39%
+        border-green"
+      >
+        <div>Content</div>
+      </div>
+    </div>
+  );
+  
     
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,51 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      width: {
+        '38%': '38%', 
+        '39%': '39%', 
+        '4/10': '40%', 
+        '51%': '51%', 
+        '51.5%': '51.5%', 
+        '52%': '52%', 
+        '8/10': '80%', 
+        '77%': '77%',
+        '78%': '78%', 
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.border-red': {
+          border: '.1px solid red',
+        },
+        '.border-green': {
+          border: '.1px solid green',
+        },
+        '.border-blue': {
+          border: '.1px solid blue',
+        },
+        '.border-yellow': {
+          border: '.1px solid yellow',
+        },
+        '.border-purp': {
+          border: '.1px solid purple',
+        },
+        '.border-oj': {
+          border: '.1px solid orange',
+        },
+        '.border-pink': {
+          border: '.1px solid pink',
+        },
+        '.border-white': {
+          border: '.1px solid white',
+        },
+        '.border-d-white': {
+          border: '.1px dashed white',
+        },
+      }
+      addUtilities(newUtilities)
+    }
+  ],
 }
