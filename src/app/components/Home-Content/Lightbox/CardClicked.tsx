5651ab2f17e4857c1a2ea364525f35a5ae223cba
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-10-31 17:56:17 +1100

    added lightbox card clicked xs blocking

--- src/app/components/Home-Content/Lightbox/CardClicked.tsx
+++ src/app/components/Home-Content/Lightbox/CardClicked.tsx
@@ -104,52 +104,63 @@ const Card: React.FC<CardProps> = ({ card }) => {
         </div>
         <div className={`${styles.middleBorder}`}></div>
         <div className={`${styles.bottomContent}`}>
+          {/*Red Border*/}
           <div className={`${styles.cardIcons}`}>
-            <div className={`${styles.cardIcon}`}>
-              <img
-                className="w-20 pt-1"
-                src={social_capital_book.src}
-                alt="book"
-              />
+            <div class="flex">
+              <div className={`${styles.cardIcon}`}>
+                <img
+                  className="w-20 pt-1"
+                  src={social_capital_book.src}
+                  alt="book"
+                />
+              </div>
+              {/*<div id="bookName1">*/}
+              {/*  <p className={`${styles.bookName} font-bold`}>*/}
+              {/*    Social Capital: A Multifaceted Perspective*/}
+              {/*  </p>*/}
+              {/*</div>*/}
+              <div className={`${styles.cardHeartTwo}`}>
+                                                     <svg
+                                                     version="1.0"
+                                                     xmlns="http://www.w3.org/2000/svg"
+                                                     width="3em"
+                                                     height="3em"
+                                                     viewBox="0 0 752.000000 752.000000"
+                                                     preserveAspectRatio="xMidYMid meet">
+                                                     <g
+                                                     transform="translate(100.000000,752.000000) scale(0.100000,-0.100000)"
+                                                     fill="#d2cfca2b"
+                                                     stroke="none">
+                                                     <path
+                                                     d="M2496 5665 c-595 -113 -1011 -636 -982 -1235 13 -273 103 -511 274
+                                                     -728 34 -44 454 -470 933 -946 959 -956 913 -916 1039 -916 127 0 78 -43 1059
+                                                     937 489 488 909 915 934 948 368 493 334 1168 -79 1590 -237 241 -530 365
+                                                     -868 365 -135 0 -258 -18 -370 -55 -207 -67 -332 -149 -528 -343 l-148 -146
+                                                     -147 146 c-214 210 -365 302 -601 364 -125 33 -390 42 -516 19z m399 -190
+                                                     c112 -20 243 -69 347 -131 64 -38 126 -91 273 -236 115 -112 202 -189 218
+                                                     -193 54 -14 80 4 262 185 193 190 278 254 410 309 117 49 218 72 346 78 299
+                                                     15 567 -87 773 -293 300 -300 378 -759 196 -1151 -83 -178 -87 -182 -1029
+                                                     -1121 -487 -485 -896 -885 -908 -888 -13 -3 -33 -3 -45 0 -30 8 -1762 1732
+                                                     -1823 1814 -308 414 -277 985 73 1339 238 240 572 346 907 288z"
+                                                     />
+                                                     </g>
+                                                     </svg>
+                                                     </div>
             </div>
             <div className={`${styles.itemButtons}`}>
 
               <div className={`${styles.bookTitle}`}>
-                <p className={`${styles.bookName} font-bold`}>
-                  Social Capital: A Multifaceted Perspective
-                </p>
+                <div id="bookName2">
+                  <p className={`${styles.bookName} font-bold`}>
+                    Social Capital: A Multifaceted Perspective
+                  </p>
+                </div>
                 <div className={`${styles.bookAuthor} text-gray-400`}>
                   <p>By: Ismail Serageldin | Paperback | 25 September 1999</p>
                 </div>
               </div>
 
-              <div className={`${styles.cardHeartTwo}`}>
-                <svg
-                  version="1.0"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="3em"
-                  height="3em"
-                  viewBox="0 0 752.000000 752.000000"
-                  preserveAspectRatio="xMidYMid meet">
-                  <g
-                    transform="translate(100.000000,752.000000) scale(0.100000,-0.100000)"
-                    fill="#d2cfca2b"
-                    stroke="none">
-                    <path
-                      d="M2496 5665 c-595 -113 -1011 -636 -982 -1235 13 -273 103 -511 274
-                            -728 34 -44 454 -470 933 -946 959 -956 913 -916 1039 -916 127 0 78 -43 1059
-                            937 489 488 909 915 934 948 368 493 334 1168 -79 1590 -237 241 -530 365
-                            -868 365 -135 0 -258 -18 -370 -55 -207 -67 -332 -149 -528 -343 l-148 -146
-                            -147 146 c-214 210 -365 302 -601 364 -125 33 -390 42 -516 19z m399 -190
-                            c112 -20 243 -69 347 -131 64 -38 126 -91 273 -236 115 -112 202 -189 218
-                            -193 54 -14 80 4 262 185 193 190 278 254 410 309 117 49 218 72 346 78 299
-                            15 567 -87 773 -293 300 -300 378 -759 196 -1151 -83 -178 -87 -182 -1029
-                            -1121 -487 -485 -896 -885 -908 -888 -13 -3 -33 -3 -45 0 -30 8 -1762 1732
-                            -1823 1814 -308 414 -277 985 73 1339 238 240 572 346 907 288z"
-                    />
-                  </g>
-                </svg>
-              </div>
+
 
               <div className={`${styles.itemToggles}`}>
                 <div className={`${styles.itemPrice}`}>
