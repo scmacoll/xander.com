a18a879d277bd88f42fe8997218e848ba97ef8ce
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-11-18 14:09:04 +1100

    payment page blocking draft

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import PaymentPageContent from '../components/Payment-Page-Content/Payment-Page-Content';
+
+
+export default function Home() {
+  return (
+    <main>
+      <PaymentPageContent />
+    </main>
+  );
+}
