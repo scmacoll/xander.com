77b6c1901c05fef10286b135defb0a2bed4f5171
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-12-06 21:02:49 +1100

    first row of cards data added from mongo DB

--- src/app/components/Checkout-Page-Content/Checkout-Page-Content.tsx
+++ src/app/components/Checkout-Page-Content/Checkout-Page-Content.tsx
@@ -1171,7 +1171,7 @@ const CheckoutPageContent: React.FC<CardProps> = ({card}) => {
           </div>
         </div>
         <div id="rightContentWrapper"
-             className="relative border-red flex flex-col pt-1 xs:w-full sm:w-full md:w-39% lg:w-39%">
+             className="relative flex flex-col pt-1 xs:w-full sm:w-full md:w-39% lg:w-39%">
           <div id="checkoutTitle"
                className="pt-1 xs:block sm:block md:hidden lg:hidden">
             <div className="flex pb-1">
