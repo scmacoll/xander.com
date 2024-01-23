64532f47f987219cd431128b663bc23fabe8ebf9
scmacoll <85879687+scmacoll@users.noreply.github.com>
2024-01-23 19:01:15 +1100

    cardclicked - added buy now btn func

--- src/app/components/Home-Content/Lightbox/CardClicked.tsx
+++ src/app/components/Home-Content/Lightbox/CardClicked.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Content, { TileCard } from '../Content';
 import styles from './CardClicked.module.scss';
+import Link from 'next/Link';
 import Image from 'next/image';
 import yukioMishimaImage from '../../../assets/Yukio_Mishima,_1955_(cropped)-modified(1).png';
 import { useCart } from "@/app/context/CartContext";
@@ -18,7 +19,7 @@ const Card: React.FC<CardProps> = ({card, numColumns}) => {
   const portraitImageUrl = `P${card.cell_name}.png`;
   const [qty, setQty] = useState(1);
   const [qtyPrice, setQtyPrice] = useState(parseFloat(card.book_price).toFixed(2));
-  const { addToCart, clearCart } = useCart();
+  const { addToCart, clearCart, cartId } = useCart();
   const [isConfirmAddToCart, setIsConfirmAddToCart] = useState(false);
 
   const { quoteHearts, bookHearts, toggleQuoteHeart, toggleBookHeart } = useHearts();
@@ -313,11 +314,13 @@ const Card: React.FC<CardProps> = ({card, numColumns}) => {
                     d="M 25 2 C 12.309534 2 2 12.309534 2 25 C 2 37.690466 12.309534 48 25 48 C 37.690466 48 48 37.690466 48 25 C 48 12.309534 37.690466 2 25 2 z M 25 4 C 36.609534 4 46 13.390466 46 25 C 46 36.609534 36.609534 46 25 46 C 13.390466 46 4 36.609534 4 25 C 4 13.390466 13.390466 4 25 4 z M 34.988281 14.988281 A 1.0001 1.0001 0 0 0 34.171875 15.439453 L 23.970703 30.476562 L 16.679688 23.710938 A 1.0001 1.0001 0 1 0 15.320312 25.177734 L 24.316406 33.525391 L 35.828125 16.560547 A 1.0001 1.0001 0 0 0 34.988281 14.988281 z"/>
                   </svg>
               </span></div>
-            <button
-              onClick={handleClearCart}
-              className={`flex rounded p-3 w-100px h-50px justify-center items-center cursor-pointer bg-custom-red border-solid border-foreground opacity-90 font-bold text-xs hover:bg-transparent hover:border-2`}>
-              BUY NOW
-            </button>
+            <Link href={`/checkout/${cartId}`}>
+              <button
+                onClick={handleAddToCart}
+                className={`flex rounded p-3 w-100px h-50px justify-center items-center cursor-pointer bg-custom-red border-solid border-foreground opacity-90 font-bold text-xs hover:bg-transparent hover:border-2`}>
+                BUY NOW
+              </button>
+            </Link>
           </div>
 
         </div>
