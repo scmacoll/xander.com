38c925b97c57dac5f87efc1ece5949c1256b97e6
scmacoll <85879687+scmacoll@users.noreply.github.com>
2024-02-12 17:46:52 +1100

    .env - edited PUBLIC_API 2

--- netlify/functions/getCards.ts
+++ netlify/functions/getCards.ts
@@ -1,18 +1,22 @@
 // netlify/functions/getCards.ts
 
+import cards from "@/server/routes/cards";
+
 const { MongoClient } = require('mongodb');
 
 exports.handler = async (event: any, context: any) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const uri = process.env.MONGO_URI; // Ensure this is your MongoDB Atlas URI
+  console.log("MongoDB URI: ", process.env.MONGO_URI);
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   try {
+    console.log("Connecting to MongoDB Atlas with URI: ", uri);
     await client.connect();
-    const db = client.db();
+    console.log("Connected to MongoDB Atlas");
+    const db = client.db("myTargetDatabase"); // Replace "yourDatabaseName" with the actual name of your database
     const collection = db.collection('cards');
     const cards = await collection.find().toArray();
-
     return {
       statusCode: 200,
       headers: {
@@ -30,4 +34,5 @@ exports.handler = async (event: any, context: any) => {
   } finally {
     await client.close();
   }
+  console.log(`Fetched ${cards.length} cards from the database`);
 };
